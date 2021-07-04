e6ce6285bff654907d055b86df322fd89f6cfd99
Rahul Kumar 5214 <rkhalliday1@gmail.com>
2021-07-04 17:24:28 +0530

    fixed Layout and animation

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import deathImg from './../../img/d.jpg';
+import CountUp from 'react-countup';
+// import deathImg from './../../img/d.jpg';
 import './Card.css';
 
 const Card = ({ data: { confirmed, recovered, active, deaths, lastupdatedtime } }) => {
@@ -7,18 +8,48 @@ const Card = ({ data: { confirmed, recovered, active, deaths, lastupdatedtime } 
 
     return (
         <div className="container">
+
+            <h1 className="hero"> Live Covid-19 tracker </h1>
+            <h3 className="india"> India </h3>
+
             <div className="main">
-                <div className="confirmed">confirmed: {confirmed}</div>
-                <div className="recovered">recovered: {recovered}</div>
-                <div className="active">active: {active}</div>
-                <div className="deaths">
-                    <img className="d-img" src={deathImg} alt="img" />
-                    deaths: {deaths}
+
+
+                <div className="confirmed">
+                    <h2 className="title">Confirmed</h2>
+                    <h1 className="data-value">
+                        <CountUp start={0} end={confirmed} duration={1.5} separator="," />
+                    </h1>
                 </div>
+
+                <div className="recovered">
+                    <h2 className="title">recovered</h2>
+                    <h1 className="data-value">
+                        <CountUp start={0} end={recovered} duration={1.5} separator="," />
+                    </h1>
+                </div>
+
+                <div className="active">
+                    <h2 className="title">active</h2>
+                    <h1 className="data-value">
+                        <CountUp start={0} end={active} duration={1.5} separator="," />
+                    </h1>
+                </div>
+
+                <div className="deaths">
+                    <h2 className="title">deaths</h2>
+                    <h1 className="data-value">
+                        <CountUp start={0} end={deaths} duration={1.5} separator="," />
+                    </h1>
+                </div>
+
             </div>
+
+            <h2 className="lastUpdate"><span>Last Updated on: </span> {new Date(lastupdatedtime).toDateString()}</h2>
         </div>
     )
 }
+
 
 
 export default Card;
